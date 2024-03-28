a22ccaf152f3e47ce85860c585f2a2751814fdaa
AlexDuchnowski <alex_duchnowski@brown.edu>
2024-03-28 11:02:42 -0400

    added DAS

--- code/main.py
+++ code/main.py
@@ -85,6 +85,31 @@ def draw_player(
     )
 
 
+def move(
+    direction: int,
+    screen: pygame.Surface,
+    maze: List[List[str]],
+    c: cube.Cube,
+    CELL_SIZE: int,
+    BORDER: int,
+    PLAYER_X: int,
+    PLAYER_Y: int,
+) -> Tuple[List[List[str]], int, int]:
+    coords = [PLAYER_X, PLAYER_Y]
+    if coords[[1, 0, 1, 0][direction]] == [0, 20, 20, 0][direction]:
+        c.change_face(direction)
+        maze = form_maze(c)
+        render(screen, maze, CELL_SIZE, BORDER)
+        coords[[1, 0, 1, 0][direction]] = [20, 0, 0, 20][direction]
+    elif (
+        maze[PLAYER_Y + [-1, 0, 1, 0][direction]][PLAYER_X + [0, 1, 0, -1][direction]]
+        != stickers.WALL
+    ):
+        render(screen, maze, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y)
+        coords[[1, 0, 1, 0][direction]] += [-1, 1, 1, -1][direction]
+    return maze, *coords
+
+
 pygame.init()
 pygame.font.init()
 
@@ -103,11 +128,17 @@ async def main():
 
     KEY_AQUIRED = False
 
-    delay = 0.08
+    delay, increment = 0.25, 0.1
 
     my_font = pygame.font.SysFont("Arial", 30)
 
+    # key_pickup_sound = pygame.mixer.Sound("../sound/mixkit-arcade-bonus-alert-767.wav")
+    # face_rotate_sound = pygame.mixer.Sound("../sound/mixkit-water-sci-fi-bleep-902.wav")
+    # face_rotate_sound = pygame.mixer.Sound("../sound/8-bit-game-1-186975.mp3")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    clock.tick(30)
 
     # Set up cube
     c = cube.Cube()
@@ -121,6 +152,9 @@ async def main():
     screen.fill((200, 200, 200))
     render(screen, maze, CELL_SIZE, BORDER)
 
+    MOVE_LOG = [None, None, None, None]
+
+    win = False
     run = True
     while run:
         # Handle Events
@@ -128,55 +162,42 @@ async def main():
             if event.type == pygame.QUIT:
                 run = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    if PLAYER_X == 0:
-                        c.change_face(3)
-                        maze = form_maze(c)
-                        render(screen, maze, CELL_SIZE, BORDER)
-                        PLAYER_X = 20
-                    elif maze[PLAYER_Y][PLAYER_X - 1] != stickers.WALL:
-                        render(screen, maze, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y)
-                        PLAYER_X -= 1
-                elif event.key == pygame.K_RIGHT:
-                    if PLAYER_X == 20:
-                        c.change_face(1)
-                        maze = form_maze(c)
-                        render(screen, maze, CELL_SIZE, BORDER)
-                        PLAYER_X = 0
-                    elif maze[PLAYER_Y][PLAYER_X + 1] != stickers.WALL:
-                        render(screen, maze, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y)
-                        PLAYER_X += 1
-                elif event.key == pygame.K_UP:
-                    if PLAYER_Y == 0:
-                        c.change_face(0)
-                        maze = form_maze(c)
-                        render(screen, maze, CELL_SIZE, BORDER)
-                        PLAYER_Y = 20
-                    elif maze[PLAYER_Y - 1][PLAYER_X] != stickers.WALL:
-                        render(screen, maze, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y)
-                        PLAYER_Y -= 1
-                elif event.key == pygame.K_DOWN:
-                    if PLAYER_Y == 20:
-                        c.change_face(2)
-                        maze = form_maze(c)
-                        render(screen, maze, CELL_SIZE, BORDER)
-                        PLAYER_Y = 0
-                    elif maze[PLAYER_Y + 1][PLAYER_X] != stickers.WALL:
-                        render(screen, maze, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y)
-                        PLAYER_Y += 1
-                elif event.key == pygame.K_SPACE:
+                for i, key in enumerate(
+                    [pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_LEFT]
+                ):
+                    if event.key == key:
+                        MOVE_LOG[i] = time.time()
+                        maze, PLAYER_X, PLAYER_Y = move(
+                            i, screen, maze, c, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y
+                        )
+                if event.key == pygame.K_SPACE:
                     if maze[PLAYER_Y][PLAYER_X] in "0123":
+                        # face_rotate_sound.play()
                         c.rotate_adjacent_face(int(maze[PLAYER_Y][PLAYER_X]))
                         maze = form_maze(c)
                         render(screen, maze, CELL_SIZE, BORDER)
                     elif maze[PLAYER_Y][PLAYER_X] == "K":
                         KEY_AQUIRED = True
+                        # key_pickup_sound.play()
                         for i, row in enumerate(stickers.key_room_alt):
                             stickers.stickers["Y4"][i] = row
                         maze = form_maze(c)
                         render(screen, maze, CELL_SIZE, BORDER)
                     elif maze[PLAYER_Y][PLAYER_X] == "L" and KEY_AQUIRED:
                         run = False
+                        win = True
+            elif event.type == pygame.KEYUP:
+                for i, key in enumerate(
+                    [pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_LEFT]
+                ):
+                    if event.key == key:
+                        MOVE_LOG[i] = None
+        for i in range(4):
+            if MOVE_LOG[i] is not None and time.time() - MOVE_LOG[i] > delay:
+                maze, PLAYER_X, PLAYER_Y = move(
+                    i, screen, maze, c, CELL_SIZE, BORDER, PLAYER_X, PLAYER_Y
+                )
+                MOVE_LOG[i] = time.time() - delay + increment
 
         draw_player(screen, PLAYER_X, PLAYER_Y, CELL_SIZE, BORDER)
 
@@ -184,27 +205,28 @@ async def main():
         # time.sleep(delay)
         await asyncio.sleep(0)
 
-    screen.fill((200, 200, 200))
-    you_win = my_font.render("You win!", False, (0, 0, 0))
-    answer_is = my_font.render("The answer is:", False, (0, 0, 0))
-    answer = my_font.render("[ANSWER]", False, (183, 18, 52))
-    bye_now = my_font.render("Bye now...", False, (0, 0, 0))
+    if win:
+        screen.fill((200, 200, 200))
+        you_win = my_font.render("You win!", False, (0, 0, 0))
+        answer_is = my_font.render("The answer is:", False, (0, 0, 0))
+        answer = my_font.render("[ANSWER]", False, (183, 18, 52))
+        bye_now = my_font.render("Bye now...", False, (0, 0, 0))
 
-    screen.blit(you_win, (CELL_SIZE * 9 + BORDER, CELL_SIZE * 9 + BORDER))
-    screen.blit(answer_is, (CELL_SIZE * 5 + BORDER, CELL_SIZE * 10 + BORDER))
-    screen.blit(answer, (CELL_SIZE * 12 + BORDER, CELL_SIZE * 10 + BORDER))
-    pygame.display.update()
+        screen.blit(you_win, (CELL_SIZE * 9 + BORDER, CELL_SIZE * 9 + BORDER))
+        screen.blit(answer_is, (CELL_SIZE * 5 + BORDER, CELL_SIZE * 10 + BORDER))
+        screen.blit(answer, (CELL_SIZE * 12 + BORDER, CELL_SIZE * 10 + BORDER))
+        pygame.display.update()
 
-    await asyncio.sleep(15)
+        await asyncio.sleep(5)
 
-    screen.blit(bye_now, (CELL_SIZE * 9 + BORDER, CELL_SIZE * 11 + BORDER))
-    pygame.display.update()
+        screen.blit(bye_now, (CELL_SIZE * 9 + BORDER, CELL_SIZE * 11 + BORDER))
+        pygame.display.update()
 
-    await asyncio.sleep(2)
+        await asyncio.sleep(2)
 
-    screen.fill((0, 0, 0))
-    screen.blit(answer, (CELL_SIZE * 12 + BORDER, CELL_SIZE * 10 + BORDER))
-    pygame.display.update()
+        screen.fill((0, 0, 0))
+        screen.blit(answer, (CELL_SIZE * 12 + BORDER, CELL_SIZE * 10 + BORDER))
+        pygame.display.update()
 
     pygame.quit()
 
---
+++ feedback.md
@@ -0,0 +1,19 @@
+# Small Concrete Stuff [BAD]
+- Colorblind mode would be good
+- Add DAS
+- Sprites
+- Reset command
+- Commands explanation
+
+# Bigger Picture Stuff [BAD]
+- Unlocking Controls
+- Not great that you can solve without leaving the white face
+- Plugging in the solution once it's been retrieved from a solver is somewhat underwhelming
+- Providing a goal...?
+
+# Bigger Picture Stuff [GOOD]
+- Having to use buttons to leave the starting location
+- The maze is simple
+- Color pallete / simplicity
+- Figuring out that it's a Rubik's Cube
+- Exploration to figure things out
